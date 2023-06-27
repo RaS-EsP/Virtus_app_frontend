5c215b911633ac9e2d37bea70fa6559122801448
ras5esp <jaime5alvarez5h@gmail.com>
2023-06-27 12:09:01 +0200

    Changing a title

--- src/components/trainer/views/Create_exercises/components/RenderExerciseList.tsx
+++ src/components/trainer/views/Create_exercises/components/RenderExerciseList.tsx
@@ -90,7 +90,7 @@ export const RenderExerciseList = ({
               htmlFor="Search for a exercise"
               className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-FirstColor peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
-              Search for a category
+              Search for categories
             </label>
             {isSelectCategoryOpen && (
               <ul className="bg-white overflow-y-auto  max-h-40 border-2 mt-1  rounded-md">
