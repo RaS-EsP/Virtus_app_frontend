8681cbbf244d9a2370d99e7a15f87f01152f39c4
RaS_EsP <rassel.habbo@gmail.com>
2023-07-11 12:08:52 +0200

    adding keyboard control with keys

--- src/components/trainer/views/Training Templates/components/RenderTrainingTemplate.tsx
+++ src/components/trainer/views/Training Templates/components/RenderTrainingTemplate.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Reorder, motion } from "framer-motion";
 import { TrainingDetailsInTemplate } from "../create_training";
 import { CrossIcon } from "../../../../../img/icons/crossIcon";
@@ -8,6 +8,8 @@ export const RenderTrainingTemplate = ({
   RemoveFromTrainingDetails,
   setTrainingDetails,
 }: any) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const item = {
     hidden: { y: 20, opacity: 0 },
     visible: {
@@ -26,28 +28,73 @@ export const RenderTrainingTemplate = ({
       },
     },
   };
+  const handleInputTrainingDetails = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number,
+    field: string
+  ) => {
+    const newTrainingDetails = [...TrainingDetails]; // Copia el array original para evitar mutaciones directas
+    newTrainingDetails[index] = {
+      ...newTrainingDetails[index], // Copia el objeto del elemento actual
+      [field]: e.target.value, // Actualiza el campo específico con el nuevo valor
+    };
+    setTrainingDetails(newTrainingDetails); // Actualiza el estado con el nuevo array modificado
+  };
+  const moveFocus = (
+    index: number,
+    direction: "up" | "down",
+    inputElement: HTMLInputElement
+  ) => {
+    if (direction === "up" && index > 0) {
+      const newIndex = index - 1;
+      const inputElements = document.querySelectorAll<HTMLInputElement>(
+        `input[name=${inputElement.name}]`
+      );
+      inputElements[newIndex].focus();
+    } else if (direction === "down" && index < TrainingDetails.length - 1) {
+      const newIndex = index + 1;
+      const inputElements = document.querySelectorAll<HTMLInputElement>(
+        `input[name=${inputElement.name}]`
+      );
+      inputElements[newIndex].focus();
+    }
+  };
+
+  const handleKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    if (e.key === "ArrowUp") {
+      const inputElement = e.target as HTMLInputElement;
+      moveFocus(index, "up", inputElement);
+    } else if (e.key === "ArrowDown") {
+      const inputElement = e.target as HTMLInputElement;
+      moveFocus(index, "down", inputElement);
+    }
+  };
+
   return (
     <>
       <div className="relative rounded-xl mt-5 overflow-x-auto overflow-y-hidden">
-        <table className="w-full text-sm  text-center text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-FourthColor dark:bg-gray-700 dark:text-gray-400">
+        <table className="w-full text-sm   text-gray-500 dark:text-gray-400">
+          <thead className="text-xs  text-gray-700 uppercase bg-FourthColor ">
             <tr>
-              <th scope="col" className="px-6 py-3 ">
+              <th scope="col" className="px-0 py-3  ">
                 Exercise
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className="px-0 py-3  ">
                 Sets
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className="px-0 py-3 ">
                 Reps
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className="px-0 py-3 ">
                 Rir
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className="px-0 py-3 ">
                 Weight
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className="px-0 py-3 ">
                 Rest
               </th>
               <th scope="col" className=""></th>
@@ -73,27 +120,71 @@ export const RenderTrainingTemplate = ({
                 >
                   <motion.td
                     key={index}
-                    className="bg-white border-b px-6 py-4 font-semibold text-gray-900 whitespace-nowrap dark:text-white"
+                    className="bg-white  text-center border-b px-6 py-4 font-semibold text-gray-900 whitespace-nowrap dark:text-white"
                   >
                     {detail.exercise_name}
                   </motion.td>
                   <motion.td className="bg-white border-b px-6 py-4">
-                    {detail.sets}
+                    <input
+                      type="number"
+                      required
+                      className="  px-1 w-[55px] text-center focus:outline-none rounded-lg border-2 bg- focus:border-ThirdColor border-gray-200"
+                      value={detail.sets}
+                      min={0}
+                      onChange={(e) =>
+                        handleInputTrainingDetails(e, index, "sets")
+                      }
+                    />
                   </motion.td>
                   <motion.td className="bg-white border-b px-6 py-4">
-                    {detail.repetitions}
+                    <input
+                      name="repetitions" // Agrega este atributo
+                      className="px-1 w-[55px] text-center focus:outline-none rounded-lg border-2 bg- focus:border-ThirdColor border-gray-200"
+                      min={0}
+                      value={detail.repetitions}
+                      onChange={(e) =>
+                        handleInputTrainingDetails(e, index, "repetitions")
+                      }
+                      onKeyDown={(e) => handleKeyDown(e, index)} // Agrega este atributo
+                    />
                   </motion.td>
                   <motion.td className="bg-white border-b px-6 py-4">
-                    {detail.rir}
+                    <input
+                      name="rir" // Agrega este atributo
+                      className="  px-1 w-[55px] text-center focus:outline-none rounded-lg border-2 bg- focus:border-ThirdColor border-gray-200"
+                      value={detail.rir}
+                      min={0}
+                      onChange={(e) =>
+                        handleInputTrainingDetails(e, index, "rir")
+                      }
+                      onKeyDown={(e) => handleKeyDown(e, index)} // Agrega este atributo
+                    />
                   </motion.td>
                   <motion.td className="bg-white border-b px-6 py-4">
-                    {detail.weight}
+                    <input
+                      name="weight" // Agrega este atributo
+                      className="  px-1 w-[62px] text-center focus:outline-none rounded-lg border-2 bg- focus:border-ThirdColor border-gray-200"
+                      value={detail.weight}
+                      max={999}
+                      onChange={(e) =>
+                        handleInputTrainingDetails(e, index, "weight")
+                      }
+                      onKeyDown={(e) => handleKeyDown(e, index)} // Agrega este atributo
+                    />
                   </motion.td>
                   <motion.td className="bg-white border-b px-6 py-4">
-                    {detail.rest}
+                    <input
+                      className="  px-1 w-[55px] text-center focus:outline-none rounded-lg border-2 bg- focus:border-ThirdColor border-gray-200"
+                      value={detail.rest}
+                      type="number"
+                      required
+                      min={0}
+                      onChange={(e) =>
+                        handleInputTrainingDetails(e, index, "rest")
+                      }
+                    />
                   </motion.td>
                   <motion.td className="border-b">
-                    {" "}
                     <button
                       onClick={() => RemoveFromTrainingDetails(index)}
                       className="rounded-full text-white  hover:scale-105 font-bold bg-FourthColor"
